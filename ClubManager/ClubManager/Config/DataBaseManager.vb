309daa5a647a1d19f02399482a63cff1d3d18888
dantedpr <dante.de.prado@estudiantat.upc.edu>
2024-03-09 17:04:14 +0100

    DB Connection working correctly

---
+++ ClubManager/ClubManager/Config/DataBaseManager.vb
@@ -0,0 +1,38 @@
+﻿Imports System.Data.SqlClient
+
+Public Class DatabaseManager
+
+    Private connectionString As String
+
+    Public Sub New()
+
+        connectionString = "Data Source=DANTEDPR\SQLEXPRESS;Initial Catalog=ClubManager;Integrated Security=True"
+    End Sub
+
+    Public Sub ExecuteSelectQuery(query As String)
+        Try
+
+            Using connection As New SqlConnection(connectionString)
+                ' Open connection
+                connection.Open()
+
+                Using command As New SqlCommand(query, connection)
+
+                    Dim dr As SqlDataReader = command.ExecuteReader()
+                    While dr.Read()
+
+                        'Implement select
+
+                    End While
+                    dr.Close()
+                End Using
+
+                connection.Close()
+            End Using
+        Catch ex As Exception
+
+            MessageBox.Show("Error executing query: " & ex.Message)
+        End Try
+    End Sub
+End Class
+
