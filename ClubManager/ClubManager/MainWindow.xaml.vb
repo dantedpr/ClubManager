6597533614cc499a3f5b541918a10d32f45dc83f
dantedpr <dante.de.prado@estudiantat.upc.edu>
2024-03-09 16:43:14 +0100

    Initial push

---
+++ ClubManager/ClubManager/MainWindow.xaml.vb
@@ -0,0 +1,35 @@
+﻿Class MainWindow
+
+    Public Property username As String
+    Private Property pass As String
+
+    Public Sub New()
+
+        ' Esta llamada es exigida por el diseñador.
+        InitializeComponent()
+        Load()
+        ' Agregue cualquier inicialización después de la llamada a InitializeComponent().
+
+    End Sub
+
+    Public Sub Load()
+
+        AddHandler btnClose.Click, AddressOf Close
+
+    End Sub
+
+
+    Public Overloads Sub Close()
+        System.Windows.Application.Current.Shutdown()
+    End Sub
+
+    Public Overloads Sub Minimize()
+        WindowState = WindowState.Minimized
+    End Sub
+
+    Public Sub AllowMovement(sender As Object, e As MouseEventArgs)
+        If e.LeftButton = MouseButtonState.Pressed Then
+            DragMove()
+        End If
+    End Sub
+End Class
