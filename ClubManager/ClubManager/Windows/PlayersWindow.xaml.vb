62ad9ce1bb843a2a0e337ac11531f7d8b13d5bb3
dantedpr <dante.de.prado@estudiantat.upc.edu>
2024-06-08 10:23:46 +0200

    Filtrar jugadores

--- ClubManager/ClubManager/Windows/PlayersWindow.xaml.vb
+++ ClubManager/ClubManager/Windows/PlayersWindow.xaml.vb
@@ -1,4 +1,5 @@
 ﻿Imports System.Data
+Imports System.Windows.Forms
 
 Class PlayersWindow
 
@@ -23,7 +24,45 @@ Class PlayersWindow
         BUT_NewPlayer.ImageName = "plus.png"
         BUT_NewPlayer.ButText = "Añadir jugador"
 
+        AddHandler BUT_Delete.Click, AddressOf DeletePlayer
+        BUT_Delete.Background = New SolidColorBrush(Colors.White)
+        BUT_Delete.GetBackground = New SolidColorBrush(Colors.WhiteSmoke)
+        BUT_Delete.ImageName = "delete.png"
+        BUT_Delete.ButText = "Eliminar plantilla"
 
+        AddHandler BUT_Search.Click, AddressOf Search
+        BUT_Search.Background = New SolidColorBrush(Colors.White)
+        BUT_Search.GetBackground = New SolidColorBrush(Colors.White)
+        BUT_Search.ImageName = "transparency.png"
+        BUT_Search.ButText = "Buscar"
+
+        'AddHandler Me.Info_Grid.DG.MouseDoubleClick, AddressOf EditPlayer
+
+
+        playerCategory.Items.Clear()
+        playerCategory.Items.Add("")
+        playerCategory.Items.Add("Promesas")
+        playerCategory.Items.Add("Prebenjamín")
+        playerCategory.Items.Add("Benjamín")
+        playerCategory.Items.Add("Alevín")
+        playerCategory.Items.Add("Infantil")
+        playerCategory.Items.Add("Cadete")
+        playerCategory.Items.Add("Juvenil")
+        playerCategory.Items.Add("Amateur")
+        playerCategory.SelectedIndex = 0
+        AddHandler playerCategory.SelectionChanged, AddressOf Search
+        AddHandler playerName.PreviewKeyDown, AddressOf Search2
+        AddHandler playerAge.PreviewKeyDown, AddressOf Search2
+        AddHandler playerStatus.Click, AddressOf Search
+
+
+        Dim li = Club.GetTeamsName()
+        playerTeam.Items.Clear()
+        playerTeam.Items.Add("")
+        For Each x In li
+            playerTeam.Items.Add(x)
+        Next
+        playerTeam.SelectedIndex = 0
         LoadPlayersClub()
 
     End Sub
@@ -47,23 +86,68 @@ Class PlayersWindow
 
     End Sub
 
-    Public Sub EditMyClub()
+    Private Sub CreatePlayer(sender As Object, e As RoutedEventArgs)
+        Dim w As New EditTeam
 
-        Dim w = FrameWindow.Instance
-        w.CleanWindow()
-        Dim w1 As New EditClub
-        w.Content.Children.Add(w1)
+        w.ShowDialog()
+        e.Handled = True
 
-        w1.Load()
+        LoadPlayersClub()
     End Sub
 
-    Public Sub CreatePlayer()
-
+    Private Sub Search(sender As Object, e As RoutedEventArgs)
+        LoadPlayersClub()
+        e.Handled = True
     End Sub
 
-    Public Sub RegisterAccount()
+    Private Sub Search2(sender As Object, e As Input.KeyEventArgs)
+        If e.Key = Key.Return Then
+            LoadPlayersClub()
+        End If
+    End Sub
 
+    Private Sub EditTeam(sender As Object, e As RoutedEventArgs)
 
+        Dim dg = Me.Info_Grid.DG
+
+        If dg.SelectedItems.Count > 0 Then
+            Dim drv = Me.Info_Grid.DG.SelectedItem
+            Dim team As New Team()
+            team.LoadTeam(drv.ID)
+            Dim w As New EditTeam(team)
+
+            w.ShowDialog()
+            e.Handled = True
+
+        End If
+
+        LoadPlayersClub()
+    End Sub
+
+    Private Sub DeletePlayer(sender As Object, e As RoutedEventArgs)
+        Dim dg = Me.Info_Grid.DG
+
+        If dg.SelectedItems.Count > 0 Then
+            If MessageBox.Show("¿Está seguro que desea eliminar el jugador seleccionado?", "Eliminar jugador", CType(MessageBoxButton.YesNo, MessageBoxButtons), CType(MessageBoxImage.Information, MessageBoxIcon)) = MessageBoxResult.Yes Then
+                Dim drv = Me.Info_Grid.DG.SelectedItem
+                'Dim team As New Team()
+                'team.LoadTeam(drv.ID)
+                'team.DeleteTeam()
+                e.Handled = True
+
+                LoadPlayersClub()
+            Else
+
+                e.Handled = True
+                Exit Sub
+            End If
+
+            e.Handled = True
+        Else
+            MessageBox.Show("Debe seleccionar primero un jugador de la tabla.", "Eliminar jugador", CType(MessageBoxButton.OK, MessageBoxButtons), CType(MessageBoxImage.Information, MessageBoxIcon))
+        End If
+
+        e.Handled = True
     End Sub
 
 End Class
